b788cbf5050b2455d5aa85cfcee79465198c3f0e
Jonandan <jonandanmwesigwa@gmail.com>
2025-06-18 15:55:34 +0300

    added task1 submodule

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int age = 20;
+     float height = 5.9;
+     double distance = 12345.6789;
+     char initial = 'A';
+     printf("%d\n",age);
+     printf("%f\n",height);
+     printf("%lf\n",distance);
+     printf("%c",initial);
+    return 0;
+}
